285420a6d536cae547ae96b91ddb43b92920cea9
Max Chuyko <Skovorodka150@mail.ru>
2023-02-24 00:51:10 +0300

    transition from act1 to act2 (startActivityForResult)

--- app/src/main/java/com/example/prac2/MainActivity.java
+++ app/src/main/java/com/example/prac2/MainActivity.java
@@ -1,10 +1,15 @@
 package com.example.prac2;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,10 +17,31 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        AppCompatButton bt = findViewById(R.id.act1_btn);
+        EditText et = findViewById(R.id.act1_et);
+
+        bt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.i("MainActivity","Кнопка входа была нажата");
+                Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+                intent.putExtra("name", et.getText().toString());
+                startActivityForResult(intent, 0);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        EditText et = findViewById(R.id.act1_et);
+        et.setVisibility(View.INVISIBLE);
+        TextView name = findViewById(R.id.act1_txt);
+        name.setText("Здравствуйте, " + data.getStringExtra("name") + " !");
     }
 
     public void onClick(View view) {
-        Intent intent = new Intent(this, SecondActivity.class);
-        startActivity(intent);
+        Log.i("MainActivity","Кнопка входа была нажата");
     }
 }
--- app/src/main/java/com/example/prac2/SecondActivity.java
+++ app/src/main/java/com/example/prac2/SecondActivity.java
@@ -1,12 +1,17 @@
 package com.example.prac2;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
 import androidx.appcompat.widget.SwitchCompat;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.CompoundButton;
 import android.widget.RelativeLayout;
 import android.widget.Switch;
+import android.widget.TextView;
 
 public class SecondActivity extends AppCompatActivity {
 
@@ -14,6 +19,10 @@ public class SecondActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+
+        String name = getIntent().getStringExtra("name");
+        TextView tv = findViewById(R.id.act2_name);
+        tv.setText(name);
 
         SwitchCompat sw = findViewById(R.id.act2_switch1);
         RelativeLayout rl = findViewById(R.id.act2_rl);
@@ -30,6 +39,19 @@ public class SecondActivity extends AppCompatActivity {
                 }
             }
         });
+
+        Button bt = findViewById(R.id.act2_btn);
+        bt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent cb_intent = new Intent();
+                cb_intent.putExtra("name", name);
+                setResult(RESULT_OK, cb_intent);
+                finish();
+            }
+        });
+
+
     }
 
 
