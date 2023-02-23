bfc7813fcbe7bb880fbe6b7ce836b7562da84222
Max Chuyko <Skovorodka150@mail.ru>
2023-02-23 19:39:50 +0300

    visual change with switch on SecondActivity

--- app/src/main/java/com/example/prac2/SecondActivity.java
+++ app/src/main/java/com/example/prac2/SecondActivity.java
@@ -1,8 +1,12 @@
 package com.example.prac2;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SwitchCompat;
 
 import android.os.Bundle;
+import android.widget.CompoundButton;
+import android.widget.RelativeLayout;
+import android.widget.Switch;
 
 public class SecondActivity extends AppCompatActivity {
 
@@ -10,5 +14,23 @@ public class SecondActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+
+        SwitchCompat sw = findViewById(R.id.act2_switch1);
+        RelativeLayout rl = findViewById(R.id.act2_rl);
+        sw.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                if(b){
+                    rl.setBackgroundResource(R.drawable.waitbg);
+                    sw.setText(R.string.sw_find);
+                }
+                else{
+                    rl.setBackgroundResource(R.drawable.bgprofile);
+                    sw.setText(R.string.switch_status);
+                }
+            }
+        });
     }
+
+
 }
