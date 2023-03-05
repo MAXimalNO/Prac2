6617ad5f8e913e732f9052ffa5d063cf4eb25c49
Max Chuyko <Skovorodka150@mail.ru>
2023-03-05 03:16:37 +0300

    Programm adding AuthFragment

--- app/src/main/java/com/example/prac2/MainActivity.java
+++ app/src/main/java/com/example/prac2/MainActivity.java
@@ -18,30 +18,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        AppCompatButton bt = findViewById(R.id.act1_btn);
-        EditText et = findViewById(R.id.act1_et);
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .setReorderingAllowed(true)
+                    .add(R.id.fragmentContainerView, AuthFragment.class, null)
+                    .commit();
+        }
 
-        bt.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Log.i("MainActivity","Кнопка входа была нажата");
-                Intent intent = new Intent(MainActivity.this, SecondActivity.class);
-                intent.putExtra("name", et.getText().toString());
-                startActivityForResult(intent, 0);
-            }
-        });
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        EditText et = findViewById(R.id.act1_et);
-        et.setVisibility(View.INVISIBLE);
-        TextView name = findViewById(R.id.act1_txt);
-        name.setText("Здравствуйте, " + data.getStringExtra("name") + " !");
-    }
-
-    public void onClick(View view) {
-        Log.i("MainActivity","Кнопка входа была нажата");
     }
 }
