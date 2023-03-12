e4949fbbac74032135e1bbb7a36e542f934b995a
Max Chuyko <Skovorodka150@mail.ru>
2023-03-12 18:24:09 +0300

    RecyclerView with 200 elements

--- app/src/main/java/com/example/prac2/AuthFragment.java
+++ app/src/main/java/com/example/prac2/AuthFragment.java
@@ -26,14 +26,14 @@ public class AuthFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Toast.makeText(getContext(), "onCreate", Toast.LENGTH_LONG ).show();
+        //Toast.makeText(getContext(), "onCreate", Toast.LENGTH_LONG ).show();
         Log.d(TAG, "onCreate");
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        Toast.makeText(getContext(), "onCreateView", Toast.LENGTH_LONG ).show();
+        //Toast.makeText(getContext(), "onCreateView", Toast.LENGTH_LONG ).show();
         Log.d(TAG, "onCreateView");
 
         View viewfr = inflater.inflate(R.layout.fragment_auth, container, false);
@@ -73,63 +73,63 @@ public class AuthFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        Toast.makeText(getContext(), "onViewCreated", Toast.LENGTH_LONG ).show();
+        //Toast.makeText(getContext(), "onViewCreated", Toast.LENGTH_LONG ).show();
         Log.d(TAG, "onViewCreated");
     }
 
     @Override
     public void onViewStateRestored(@Nullable Bundle savedInstanceState) {
         super.onViewStateRestored(savedInstanceState);
-        Toast.makeText(getContext(), "onViewStateRestored", Toast.LENGTH_LONG ).show();
+        //Toast.makeText(getContext(), "onViewStateRestored", Toast.LENGTH_LONG ).show();
         Log.d(TAG, "onViewStateRestored");
     }
 
     @Override
     public void onStart() {
         super.onStart();
-        Toast.makeText(getContext(), "onStart", Toast.LENGTH_LONG ).show();
+        //Toast.makeText(getContext(), "onStart", Toast.LENGTH_LONG ).show();
         Log.d(TAG, "onStart");
     }
 
     @Override
     public void onResume() {
         super.onResume();
-        Toast.makeText(getContext(), "onResume", Toast.LENGTH_LONG ).show();
+        //Toast.makeText(getContext(), "onResume", Toast.LENGTH_LONG ).show();
         Log.d(TAG, "onResume");
     }
 
     @Override
     public void onPause() {
         super.onPause();
-        Toast.makeText(getContext(), "onPause", Toast.LENGTH_LONG ).show();
+        //Toast.makeText(getContext(), "onPause", Toast.LENGTH_LONG ).show();
         Log.d(TAG, "onPause");
     }
 
     @Override
     public void onStop() {
         super.onStop();
-        Toast.makeText(getContext(), "onStop", Toast.LENGTH_LONG ).show();
+        //Toast.makeText(getContext(), "onStop", Toast.LENGTH_LONG ).show();
         Log.d(TAG, "onStop");
     }
 
     @Override
     public void onSaveInstanceState(@NonNull Bundle outState) {
         super.onSaveInstanceState(outState);
-        Toast.makeText(getContext(), "onSaveInstanceState", Toast.LENGTH_LONG ).show();
+        //Toast.makeText(getContext(), "onSaveInstanceState", Toast.LENGTH_LONG ).show();
         Log.d(TAG, "onSaveInstanceState");
     }
 
     @Override
     public void onDestroyView() {
         super.onDestroyView();
-        Toast.makeText(getContext(), "onDestroyView", Toast.LENGTH_LONG ).show();
+        //Toast.makeText(getContext(), "onDestroyView", Toast.LENGTH_LONG ).show();
         Log.d(TAG, "onDestroyView");
     }
 
     @Override
     public void onDestroy() {
         super.onDestroy();
-        Toast.makeText(getContext(), "OnDestroy", Toast.LENGTH_LONG ).show();
+        //Toast.makeText(getContext(), "OnDestroy", Toast.LENGTH_LONG ).show();
         Log.d(TAG, "onDestroy");
     }
 
---
+++ app/src/main/java/com/example/prac2/MyRecyclerAdapter.java
@@ -0,0 +1,62 @@
+package com.example.prac2;
+
+import android.content.Context;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class MyRecyclerAdapter extends RecyclerView.Adapter<MyRecyclerAdapter.ViewHolder>{
+
+    private LayoutInflater inflater;
+    private List<String> list;
+
+    MyRecyclerAdapter(Context context, List<String> list) {
+        this.list = list;
+        this.inflater = LayoutInflater.from(context);
+    }
+
+    public final static class ViewHolder extends RecyclerView.ViewHolder {
+
+        TextView message;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            message = itemView.findViewById(R.id.recycler_txt);
+        }
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View item = inflater.inflate(R.layout.recycler_item,parent,false);
+        return new ViewHolder(item);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        String message = list.get(position);
+        holder.message.setText(message);
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String mes = ((TextView)view.findViewById(R.id.recycler_txt)).getText().toString();
+                Toast.makeText(holder.itemView.getContext(), mes, Toast.LENGTH_SHORT).show();
+                Log.i("RecyclerView",mes);
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return list.size();
+    }
+}
